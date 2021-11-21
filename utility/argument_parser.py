c97f358b2cafb193945df2894697b7e2dba96a4e
Noah Mauthe <s8nomaut@stud.uni-saarland.de>
2021-11-21 18:49:16 +0100

    ArgumentParser: Split parsing according to functionality.
    In accordance with the newly subclassed Manager and the new database functionality, the parser now supports different arguments depending on the context.

--- utility/argument_parser.py
+++ utility/argument_parser.py
@@ -1,33 +1,35 @@
 import argparse
-import sys
 
-import database
+from analysis import androzoo_analysis, gplay_analysis
+from database import create_db
 
 
 def parse_args():
+    parent = argparse.ArgumentParser(add_help=False)
+    parent.add_argument('--loglevel', choices=['info', 'verbose', 'debug'], default='info',
+                        help='Specifies the global log level.')
+    parent.add_argument('--logfile', type=str, help='Specifies the logfile to use. Will append, not overwrite')
+    parent.add_argument('--db', type=str, help='Changes the default database string')
     parser = argparse.ArgumentParser()
-    parser.add_argument('--verbose', action='store_true', help='Enables verbose logging')
-    parser.add_argument('--debug', action='store_true', help='Enables debug output, prioritized over --verbose')
-    parser.add_argument('--logfile', type=str, help='Specifies the logfile to use. Will append, not overwrite')
-    parser.add_argument('--createdb', action='store_true', default=False,
-                        help='Create the database instead of running the analysis.')
-    if '--createdb' in sys.argv:
-        parser.add_argument('csv', type=str, help='The csv file to create the database from.')
-        return parser.parse_args()
-    parser.add_argument('--db', type=str, help='Changes the default database string')
-    source_group = parser.add_mutually_exclusive_group(required=True)
-    source_group.add_argument('--androzoo', type=str, help='Specifies location of androzoo API Key')
-    source_group.add_argument('--local', type=str, help='Specifies the basepath for all local files')
-    parser.add_argument('--queries', type=str, help='Specifies a file containing partial SQL queries that determine'
-                                                    ' which files will be analyzed', default=None)
-    parser.add_argument('--vt', type=str, help='Specifies location of VirusTotal API Key', default=None)
-    parser.add_argument('--vt-quota', dest='quota', type=int, help='Specifies the VirusTotal API quota already used',
-                        default=0)
-    parser.add_argument('--model', dest='model', type=str, help='Specifies the path to a model for anomaly detection',
-                        default=None)
-    args = parser.parse_args()
-    if args.db:
-        database.db_string = args.db
-    else:
-        database.db_string = 'dbname=malware user=postgres host=0.0.0.0'
-    return args
+    subparsers = parser.add_subparsers()
+    create = subparsers.add_parser('createdb', help='Creates the database containing information gathered from'
+                                                    ' the androzoo dataset.', parents=[parent])
+    create.set_defaults(func=create_db)
+    create.add_argument('--sample', action='store_true', default=False, help='If set, creates a random sample and saves'
+                                                                             ' it to "file". Otherwise the contents of'
+                                                                             ' "file" will be read and saved to db.')
+    create.add_argument('file', help='The file to take the samples from / store the samples in.')
+    androzoo = subparsers.add_parser('androzoo', help='Runs the analysis on apps from the androzoo dataset.',
+                                     parents=[parent])
+    androzoo.add_argument('--vt', type=str, help='Specifies location of VirusTotal API Key', default=None)
+    androzoo.add_argument('--vt-quota', dest='quota', type=int, help='Specifies the VirusTotal API quota already used',
+                          default=0)
+    androzoo.add_argument('key', type=str, help='Specifies the location of the androzoo key file.')
+    androzoo.add_argument('queries', type=str, help='Specifies a file containing SQL queries that determine which files'
+                                                    ' will be analyzed')
+    androzoo.set_defaults(func=androzoo_analysis)
+    gplay = subparsers.add_parser('gplay', help='Runs the analysis on local apps from the GooglePlay dataset.',
+                                  parents=[parent])
+    gplay.add_argument('root', type=str, help='Specifies the directory root of all .apk files.')
+    gplay.set_defaults(func=gplay_analysis)
+    return parser.parse_args()
