8e3a864f9569033092dcc37a5663379d6afc6b90
Noah Mauthe <s8nomaut@stud.uni-saarland.de>
2021-12-15 01:13:18 +0100

    ApkManagers: Fixed apk discovery for fdroid

--- apk_managers/local.py
+++ apk_managers/local.py
@@ -15,7 +15,8 @@ class GplayApkManager(ApkManager):
 
     def __init__(self, path, queue, workers):
         super().__init__(queue, workers)
-        apks = fnmatch.filter(glob.iglob(os.path.join(path, '**'), recursive=True), '*.apk')
+        apks = fnmatch.filter(glob.iglob(os.path.join(os.path.expanduser(os.path.abspath(path)), '**'), recursive=True),
+                              '*.apk')
         done = set(e[0] for e in database.access('SELECT sha256 from results UNION SELECT sha256 from errors;'))
         self.logger.info(f'Found {len(done)} already processed apks.')
         dirs = [os.path.dirname(apk) for apk in apks]
@@ -38,18 +39,20 @@ class FDroidApkManager(ApkManager):
 
     def __init__(self, path, queue, workers):
         super().__init__(queue, workers)
-        apk_names = fnmatch.filter(glob.iglob(os.path.join(path, '**'), recursive=True), '*.apk')
+        self.logger.info(path)
+        apk_paths = fnmatch.filter(glob.iglob(os.path.join(os.path.expanduser(os.path.abspath(path)), '**'),
+                                              recursive=True), '*.apk')
         done = set(e[0] for e in database.access('SELECT sha256 from results UNION SELECT sha256 from errors;'))
         self.logger.info(f'Found {len(done)} already processed apks.')
-        dirs = [os.path.dirname(apk) for apk in apk_names]
-        hashes = [sha256sum(apk) for apk in apk_names]
+        dirs = [os.path.dirname(apk) for apk in apk_paths]
+        hashes = [sha256sum(apk) for apk in apk_paths]
         apks = []
-        for h, d, c, r, n in zip(hashes, dirs, repeat(store_fdroid_apk_info), repeat(clean.fdroid_remnants), apk_names):
+        for h, d, c, r, n in zip(hashes, dirs, repeat(store_fdroid_apk_info), repeat(clean.fdroid_remnants), apk_paths):
             if h not in done:
                 apks.append((h, d, c, r))
                 with open(os.path.join(path, h), 'w') as file:
                     file.write(n.split('.apk')[0] + '\n')
-        self.logger.info(f'Initialized with {len(apk_names)} apks.')
+        self.logger.info(f'Initialized with {len(apks)} apks.')
         self.apks = iter(apks)
 
     def next_apk(self):
